1859335f8b2c6155f7f23021b7e5f45227a6480c
sofiehyllen <sofiehyllen@hotmail.com>
2023-10-18 11:05:47 +0200

    3 hovedsider + nav

--- src/pages/SleeptrackPage.jsx
+++ src/pages/SleeptrackPage.jsx
@@ -1,5 +1,3 @@
-import Audiofile from '../components/Audiofile';
-
 
 export default function SleeptrackPage(){
 
@@ -7,31 +5,6 @@ export default function SleeptrackPage(){
         <section className="page-content">
             <p className="titel1">Sleeptrack content</p>
 
-            <Audiofile
-            height={40}
-            waveColor="rgba(96, 92, 110)"
-            progressColor="rgba(240, 238, 230)"
-            url={'src/assets/audio-rain.mp3'}
-            barHeight={1.5}
-            barWidth={3}
-            barGap={3}
-            barRadius={10}
-            dragToSeek={true}
-            />
-    
-
-            <Audiofile
-            height={40}
-            waveColor="rgba(96, 92, 110)"
-            progressColor="rgba(240, 238, 230, 1)"
-            url={'src/assets/audio-ocean-waves.mp3'}
-            barHeight={2}
-            barWidth={3}
-            barGap={3}
-            barRadius={10}
-            dragToSeek={true}
-            />
-       
         </section>
     )
 }
