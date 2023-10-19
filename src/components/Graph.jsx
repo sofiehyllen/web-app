9f7be681ce462ec2c4242365dacb4d284fba6549
sofiehyllen <sofiehyllen@hotmail.com>
2023-10-19 10:22:09 +0200

    graph - semi stylet

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -3,6 +3,7 @@ import {Bar} from 'react-chartjs-2';
 import 'chart.js/auto'
 import 'react-chartjs-2' 
 
+
 const state = {
   labels: ['M', 'T', 'W','T', 'F', 'S', 'S'],
   datasets: [
@@ -16,14 +17,45 @@ const state = {
 
 export default function Graph() {
     return (
-      <div>
+      <div className='graph'>
         <Bar
           data={state}
           options={{
-            layout: {
+            
+            responsive:true, //Gør grafen responsive
+            maintainAspectRatio: false, //Gør at grafen ikke forbliver lille hvis viewporten forstørres 
+            plugins:{ 
+                legend:false, //Fjerner overskrift
+            }, 
+            scales:{
                 
-            },
-            plugins:{legend:false} //Fjerner overskrift
+                x: {
+                    grid:{display:false},
+                    ticks:{
+                        color:'#F0EEE6',
+                        font:{
+                            family: 'mundial',
+                            size: 15,
+                        }
+                    }
+                },
+                y: {
+                    grid:{
+                        color: 'rgba(67, 55, 112)',
+                        lineWidth: 2,
+
+                    },
+                    ticks:{
+                        stepSize: 2,
+                        color: 'rgba(240, 238, 230, 0.7)',
+                        font:{
+                            family: 'mundial',
+                            weight: 100,
+                            size: 12,
+                        }
+                    }
+                }
+            }
           }}/>
       </div>
     );
