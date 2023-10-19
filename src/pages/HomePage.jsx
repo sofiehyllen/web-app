b205996690bb14b5912686b725d306e8901101ee
sofiehyllen <sofiehyllen@hotmail.com>
2023-10-19 18:20:09 +0200

    rettelser i klasser og layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,30 @@
-import Graph from "../components/Graph";
+import { Link } from "react-router-dom";
 
 
 export default function HomePage(){
     return(
         <section className="page-content">
-      <p id="home-date" className="heading heading-small">October 11</p>
-         <p className="titel">Homepage content</p>
+            <h6 id="home-date" className="heading heading-small">October 11</h6>
+            <h1 className="titel">Good evening <br /><span className="titel-tab">Sarah</span></h1>
             
-         <div className="brickcontainer large">
-            <p className="heading">Ready to go to sleep?</p>
-            <p className="bodytext">Start the sleeptracker right before you go to sleep to keep track of the hours you sleep</p>
-         </div>
+            <div className="brickcontainer large">
+                <h2 className="heading">Ready to go to sleep?</h2>
+                <p className="bodytext">Start the sleeptracker right before you go to sleep to keep track of the hours you sleep</p>
+                <div className="button btn-small"><Link to="/sleeptrackpage">go to sleep</Link></div>
+            </div>
 
-         <div className="brickcontainer medium">
-            <p className="heading">Your statistics</p>
-            <p className="bodytext">Explore the world of your personal sleep trackings</p>
-         </div>
 
-         <div className="brickcontainer">
-            <p className="heading">Sounds</p>
-            <p className="bodytext">Fall asleep to one of many calmning sleeping sounds</p>
-         </div>
+            <div className="flex">
+                <div className="brickcontainer">
+                    <h2 className="heading">Sounds</h2>
+                    <p className="bodytext">Fall asleep to one of many calmning sleeping sounds</p>
+                </div>
+
+                <div className="brickcontainer">
+                    <h2 className="heading">Statistics</h2>
+                    <p className="bodytext">Keep track of your sleep with the statistics of your latest sleepcycle</p>
+                </div>
+            </div>
         </section>
     )
 }
