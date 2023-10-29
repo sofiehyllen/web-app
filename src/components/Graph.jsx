67b4f5f1bf825cff54d1f31493b523b03e614b64
sofiehyllen <sofiehyllen@hotmail.com>
2023-10-29 20:23:02 +0100

    graf med statistik

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,77 +1,115 @@
-import 'react';
-import {Bar} from 'react-chartjs-2';
-import 'chart.js/auto'
-import 'react-chartjs-2' 
-import Chart from 'chart.js/auto';
+import { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import 'chart.js/auto';
+import 'react-chartjs-2';
+import moment from 'moment';
 
-const state = {
-  labels: ['M', 'T', 'W','T', 'F', 'S', 'S'],
-  datasets: [
-    {
-        label: 'Sleepgraph',
+
+export default function Graph() {
+    const [state, setState] = useState({
+    labels: [],
+    datasets: [
+        {
+        label: "Sleepdata",
         backgroundColor: 'rgba(231, 115, 96, 1)',
         borderRadius: '8',
-        data: [6.3, 7.4, 7.1, 9.5, 4.7, 6, 6.5]
-    },
-    {
+        data: [],
+        },
+        {
         label: 'Psudodata',
         backgroundColor: 'rgba(240, 238, 230, 0.1)',
         borderRadius: '8',
-        data: [10, 10, 10, 10, 10, 10, 10]
+        data: [10, 10, 10, 10, 10, 10, 10],
+        },
+    ],
+    });
+
+    const formattedDay = (date) => {
+        const dayLetter = moment(date).format("dddd").charAt(0);
+        return dayLetter;
     }
-  ]
-};
-
-Chart.defaults.datasets.bar.maxBarThickness = 28;
-Chart.defaults.font.family = 'mundial';
-Chart.defaults.font.weight = 100;
-
-export default function Graph() {
-    return (
-      <div className='spacing-top' id='graph'>
-        <Bar
-          data={state}
-          options={{
-            responsive:true, //Gør grafen responsive
-            maintainAspectRatio: false, //Gør at grafen ikke forbliver lille hvis viewporten forstørres 
-            plugins:{ 
-                legend:false, //Fjerner overskrift
-            }, 
-            scales:{
-                x: {
-                    stacked:true,
-                    border: {display:false},
-                    grid:{
-                        display:false,
-                    },
-                    ticks:{
-                        count:7,
-                        color:'rgba(240, 238, 230, 1)',
-                        font:{
-                            size: 17,
-                        }
-                    }
+  
+    useEffect(() => {
+    async function fetchData() {
+        const url = 'https://sleep-aa77c-default-rtdb.europe-west1.firebasedatabase.app/sleep.json';
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+        
+            if (data !== null) {
+            const entries = Object.entries(data);
+            const latestEntries = entries.slice(-7); // Tag de seneste 7 elementer
+        
+            const sleepData = latestEntries.map(([, entry]) => parseFloat(entry.hs)); // Konverter til tal
+            const dayData = latestEntries.map(([, entry]) => formattedDay(entry.date)); 
+            setState((prev) => ({
+                ...prev,
+                labels: dayData,
+                datasets: [
+                {
+                    ...prev.datasets[0],
+                    data: sleepData,
                 },
-                y: {
-                    beginAtZero:true,
-                    border: {display:false},
-                    grid:{
-                        color: 'rgba(67, 55, 112)',
-                        lineWidth: 2,
-                        tickLength: 0,
-                    },
-                    ticks:{
-                        padding: 5,
-                        count:6,
-                        stepSize: 2,
-                        color: 'rgba(240, 238, 230, 0.7)',
-                        font:{
-                            size: 12,
-                        }
-                    }
-                }
+                ...prev.datasets.slice(1), // behold andre datasets uændrede
+                ],
+            }));
+            } else {
+            console.log('Ingen data fundet i databasen.');
             }
-          }}/>
-      </div>
-    );
-  }
+            } catch (error) {
+            console.error('Fejl under hentning af data:', error);
+            }
+        }
+        
+    fetchData();
+    }, []);
+
+  return (
+    <div className='spacing-top' id='graph'>
+      <Bar
+        data={state}
+        options={{
+          responsive: true,
+          maintainAspectRatio: false,
+          plugins: {
+            legend: false,
+          },
+          scales: {
+            x: {
+              stacked: true,
+              border: { display: false },
+              grid: {
+                display: false,
+              },
+              ticks: {
+                count: 7,
+                color: 'rgba(240, 238, 230, 1)',
+                font: {
+                  size: 17,
+                },
+              },
+            },
+            y: {
+              beginAtZero: true,
+              border: { display: false },
+              grid: {
+                color: 'rgba(67, 55, 112)',
+                lineWidth: 2,
+                tickLength: 0,
+              },
+              ticks: {
+                padding: 5,
+                count: 6,
+                stepSize: 2,
+                color: 'rgba(240, 238, 230, 0.7)',
+                font: {
+                  size: 12,
+                },
+              },
+            },
+          },
+        }}
+      />
+    </div>
+  );
+}
