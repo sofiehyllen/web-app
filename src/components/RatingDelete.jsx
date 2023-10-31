e4571364b7e06981148a5f8c1f12387ce00b27f6
sofiehyllen <sofiehyllen@hotmail.com>
2023-10-31 12:04:32 +0100

    modal men stadig på sin egen side

--- src/components/RatingDelete.jsx
+++ src/components/RatingDelete.jsx
@@ -1,29 +1,24 @@
 import { useEffect, useState } from "react";
 import {  useNavigate, useParams } from "react-router-dom";
-
-import RatingCard from "./RatingCard";
+import DeleteModal from "./DeleteModal";
 
 
 export default function RatingDelete() {
     const navigate = useNavigate();
-  const [post, setPost] = useState({});
-  const params = useParams(); //URL indeholder parameters som er det samme som dataets ID (key)
-        // console.log(`params ${params.postId}`);
-  const url = `https://sleep-aa77c-default-rtdb.europe-west1.firebasedatabase.app/sleep/${params.postId}.json`;
+    const [post, setPost] = useState({});
+    const params = useParams(); //URL indeholder parameters som er det samme som dataets ID (key)
+    const url = `https://sleep-aa77c-default-rtdb.europe-west1.firebasedatabase.app/sleep/${params.postId}.json`;
 
-  useEffect(() => {
+    useEffect(() => { //Her hentes det aktuelle "kort" med dataen for den givne dag
     async function getPost() {
-      const response = await fetch(url);
-      const data = await response.json();
-      setPost(data);
+        const response = await fetch(url);
+        const data = await response.json();
+        setPost(data);
     }
     getPost();
-  }, [url]);
+    }, [url]);
 
-  // This function deletes the current translation.
-  // It asks the user for permission before the
-  // deletion is completed.
-  async function deletePost() {
+  async function deletePost() { //Her slettes den aktuelle tracking. Brugeren spørges igen før den slettes endeligt.
     const confirmDelete = window.confirm(`Do you want to delete this sleeptracking?`)
     
     if (confirmDelete) {
@@ -33,19 +28,17 @@ export default function RatingDelete() {
 
         const data = await response.json();
         console.log(data);
-        navigate("/");
+        navigate("/statisticspage");
       }
   }
 
-  // PostForm.js is implemented and 
-  // callback "savePost" holds the 
-  // name of the method that updates
-  // the new translation.
     return (
-        <section>
-            <h1>Delete Post</h1>
-            <RatingCard post={post} />
-            <button className="btn-delete" onClick={deletePost}>Delete Post</button>
+
+        <section className="modal">
+            <div className="modal-content">
+                <DeleteModal post={post} />
+                <button className="button btn-purple" onClick={deletePost}>Delete Post</button>
+            </div>
         </section>
     );
 }
