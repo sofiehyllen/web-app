25d68a8f39039dd7cfdb91f9e029c025f369391e
sofiehyllen <sofiehyllen@hotmail.com>
2023-10-31 14:19:30 +0100

    confirm delete modal

--- src/components/RatingDelete.jsx
+++ src/components/RatingDelete.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import {  useNavigate, useParams } from "react-router-dom";
-import DeleteModal from "./DeleteModal";
+import RatingCard from "./RatingCard";
+
 
 
 export default function RatingDelete() {
@@ -8,6 +9,7 @@ export default function RatingDelete() {
     const [post, setPost] = useState({});
     const params = useParams(); //URL indeholder parameters som er det samme som dataets ID (key)
     const url = `https://sleep-aa77c-default-rtdb.europe-west1.firebasedatabase.app/sleep/${params.postId}.json`;
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => { //Her hentes det aktuelle "kort" med dataen for den givne dag
     async function getPost() {
@@ -18,26 +20,47 @@ export default function RatingDelete() {
     getPost();
     }, [url]);
 
-  async function deletePost() { //Her slettes den aktuelle tracking. Brugeren spørges igen før den slettes endeligt.
-    const confirmDelete = window.confirm(`Do you want to delete this sleeptracking?`)
-    
-    if (confirmDelete) {
+    const handleConfirm = async () => {
         const response = await fetch(url, {
-            method: "DELETE"
-        });
+            method: 'DELETE',
+            });
 
         const data = await response.json();
-        console.log(data);
-        navigate("/statisticspage");
-      }
-  }
+            console.log(data);
+
+            setShowModal(false);
+            navigate('/statisticspage'); // Du skal måske importere navigate fra dit routingbibliotek
+    };
+
+    const handleClick = () => {
+        setShowModal(true);
+    };
+
+    const handleCancel = () => {
+       // setShowModal(false);
+        navigate('/statisticspage')
+    };
 
     return (
+        <section className="">
+            <div className="page-content">
+                <RatingCard post={post} />
+                <div className="center">
+                    <button className="button btn-small" onClick={handleClick}>Delete Post  <span className='btn-shine'/></button>
+                </div>
 
-        <section className="modal">
-            <div className="modal-content">
-                <DeleteModal post={post} />
-                <button className="button btn-purple" onClick={deletePost}>Delete Post</button>
+                {showModal && (
+                    <div className="delete-modal">
+                        <div className="delete-content">
+                            <h1 className="titel">Delete?</h1>
+                            <h2 className="heading heading-small spacing-bottom">Are you sure you want to delete the sleeptracking of this day?</h2>
+                            <div className="center">
+                                <button className="button btn-small btn-delete" onClick={handleConfirm}>Delete <span className='btn-shine'/></button>
+                                <button className="button btn-small" onClick={handleCancel}>Cancel <span className='btn-shine'/></button>
+                            </div>
+                        </div>
+                        </div>
+                )}
             </div>
         </section>
     );
