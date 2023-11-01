03eac3f16b82890981d226c2427a481d4867f3f0
sofiehyllen <sofiehyllen@hotmail.com>
2023-11-01 09:59:42 +0100

    playbutton

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -6,13 +6,13 @@ export default function Favourites({ favorites, handleHeartClick }) {
     
   return (
     <section className='favourites spacing-top relative'>
-      <p className="heading spacing-bottom"> Your favourites </p>
-      <img className="favourite-heart absolute" src="src/assets/heart-bg.svg" alt="heart icon favourites" ></img>
+      <p className="heading"> Your favourites </p>
+      {/*<img className="favourite-heart absolute" src="src/assets/heart-bg.svg" alt="heart icon favourites" ></img>*/}
       {favorites.length === 0 ? (
         <p className='heading heading-small bodytext spacing-bottom'> # Like your favorite sounds to display them here.</p>
       ) : (
         favorites.map((favorite, index) => (
-          <div key={index}>
+          <div className="spacing-top" key={index}>
             <Audiofile
               textarea={favorite.title}
               height={20}
