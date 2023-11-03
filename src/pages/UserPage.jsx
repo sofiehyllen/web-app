08868edc0f909d7161e4edf6ee81acac8a90c95d
sofiehyllen <sofiehyllen@hotmail.com>
2023-11-03 12:00:50 +0100

    rettelser

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -34,16 +34,16 @@ export default function UserPage() {
     return (
         <motion.section variants={containerAnimation} initial="hidden" animate="visible" className="page-content">
             <motion.div variants={itemAnimation}><GoBackButton /></motion.div>
-            <motion.div variants={containerAnimation}>
+            <div>
                 <motion.h1 variants={itemAnimation} className='titel titel-big'> Hello...</motion.h1>
                 <motion.p variants={itemAnimation} className='bodytext spacing-bottom'>
                     Please provide us with your name and email address so
                     that we may send you captivating updates and customize
                     the application precisely to meet your requirements.
                 </motion.p>
-            </motion.div>
+            </div>
             <form method="POST">
-                <motion.div variants={containerAnimation} className="flex">
+                <div className="flex">
                     <motion.div variants={itemAnimation}>
                         <label htmlFor="firstname"><h4 className='bodytext titelcolor'>First name</h4></label>
                         <input className='form-input heading heading-small' id="firstname" type="text" name="Firstname" value={user.Firstname} autoComplete="off" onChange={userdata} required />
@@ -53,32 +53,30 @@ export default function UserPage() {
                         <label htmlFor="lastname"><h4 className='bodytext titelcolor'>Surname</h4></label>
                         <input className='form-input heading heading-small' id="lastname" type="text" name="Lastname" value={user.Lastname} autoComplete="off" onChange={userdata} required />
                     </motion.div>
-                </motion.div>
+                </div>
 
-                <div className="spacing-bottom">
+                <motion.div variants={itemAnimation} className="spacing-bottom">
                     <label htmlFor="email"><h4 className='bodytext titelcolor'>E-mail</h4></label>
                     <input className='form-input heading heading-small' id="email" type="email" name="Email" value={user.Email} autoComplete="off" onChange={userdata} required />
-                </div>
+                </motion.div>
 
                 <div className='center'>
-                    <h2 className='heading'> Expected hours of sleep </h2>
-                    <p className='heading heading-small bodytext spacing-bottom'>
+                    <motion.h2 variants={itemAnimation} className='heading'> Expected hours of sleep </motion.h2>
+                    <motion.p variants={itemAnimation} className='heading heading-small bodytext spacing-bottom'>
                         To assist us in tracking your sleep pattern, kindly let us
                         know the number of hours you aim to sleep per night.
-                    </p>
-                    <div className="spacing-bottom">
+                    </motion.p>
+                    <motion.div variants={itemAnimation} className="spacing-bottom">
                         <label htmlFor="hoursofsleep"><h4 className='heading heading-small'>Hours of sleep</h4></label>
-
                         <input type="number" className='form-input heading heading-small' id="hoursofsleep" name="Hoursofsleep" value={user.Hoursofsleep} autoComplete="off" onChange={userdata} required />
-
-                    </div>
+                    </motion.div>
                 </div>
 
-                <div className='center spacing-bottom'>
+                <motion.div variants={itemAnimation} className='center spacing-bottom'>
                     <button onClick={getdata} type='submit' className='button btn-big'>
                         save <span className="buttonshine" />
                     </button>
-                </div>
+                </motion.div>
             </form>
         </motion.section>
     );
